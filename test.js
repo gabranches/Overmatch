7caa865235c36cb7ba9f0863d693dfc3d8625109
Gustavo Abranches <gabranches@MacBook-Pro-2.local>
2015-11-12 18:01:04 -0500

    added comments and comment deletion

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 var stats = require('./lib/stats.js')();
+var dbTransactions = require('./lib/dbTransactions.js')();
 
-var results = stats.getGraphData(0, 3, 'maps', 7);
 
-console.log(results);
+
+var comment = {
+    date: new Date(),
+    text: 'Hello this is a test comment',
+    rating: 0,
+    vote: 7,
+    user: 'Anonymous'
+}
+
+// var results = dbTransactions.writeComment(7, 0, 'friends', comment);
+
+dbTransactions.removeComment(7, 0, 'friends', '56450c926c23881323703a2c');
 
